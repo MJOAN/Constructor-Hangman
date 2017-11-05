2109b2e96ccb0b8d5eebe813c34cf7e9f920ff15
Mariam Joan <MariamJoan@Mariams-MacBook-Pro.local>
2017-11-04 23:47:17 -0700

    11-4

--- letter.js
+++ letter.js
@@ -1,83 +1,123 @@
-
 // dependency for inquirer npm package
+// access all of the exports in our word.js file
 var inquirer = require("inquirer");
 var prompt = require("prompt");
-// Using the require keyword lets us access all of the exports in our word.js file
 var word = require("./word.js");
-var letter = process.argv[2];
 
+var wordSelection = ["trace scheduling", "fortran", "formatting objects processor", 
+"fourier transform", "high level assembly", "instruction set architecture", 
+"sparse conditional constant propagation", "design compiler"];
 
+// randomized word chosen from wordOptons[]
+var chosenWord = wordSelection[Math.floor(Math.random() * wordSelection.length)];
 
+// split word and store in var
+var lettersInWord = chosenWord.split("");
 
+// number of "__ __"  so when I call placeHolder I can have it === "__"
+var placeHolder = lettersInWord.length;
 
-// Grabs the letter information
-if (process.argv[2]) {
-  var letter = process.argv[2];
+// display "__" + letterGuessed correctly
+var lettersWithPlacholders = []; 
+
+// counts how many times 'guess a letter' question occurs
+var numberOfGuesses = 25;
+
+function Letter(chosenWord, lettersGuessed, placeHolder) {
+  this.letterGuessed = letterGuessed,
+  this.placeHolder = placeHolder,
+  this.lettersWithPlacholders = lettersWithPlacholders
 }
 
-// Loop through word file in wordOptions [] and print out details
-for (var letter in word) {
-  // somehow turn randomized wordOptions array to __ __ __ __ __ __ [show/hide?]
+var startGame = function() {
+// runs inquirer and asks the user a series of questions 
+// replies are stored in variable answers inside .then statement
+// run placeholder prototype function @ start of game "__ __ " 
+  this.lettersInWord.placeHolder();  
 
-  console.log(wordOptions[i].join('__');
-}
-
-
-
-
-// variable we will use to count how many times 'guess a letter' question occurs
-var letterCount = 0;
-// array in which we will store each of our new word objects
-var wordArray = [];
-
-var askQuestion = function() {
-  // if statement to ensure that our questions are only asked word array length
-  if (count < wordArray.length) {
-  
-    // runs inquirer and asks the user a series of questions whose replies are
-    // stored within the variable answers inside of the .then statement
     inquirer.prompt([
       {
-      type: "list",
-      name: "letter",
+      type: "input",
       message: 'Guess a letter!',
-      choices: ["__", "2", "3", "4"],
-      validate: function(letter) {
-        if (isNaN(letter) === false) {
+      validate: function(value) {
+        if (isNaN(value) === true) {
             return true;
         }
           return false;
         }
       }, 
-    ]).then(function(answers) {
-      // initializes the variable newguy to be a programmer object which will
-      // take in all of the user's answers to the questions above
-      var letterGuesses = new Word(answers.letter,answers.position);
+    ]).then(function(err, res) {
+      // initializes the variable to be an object user answers to questions
+      var letter = new Letter(res.letterGuessed);
 
-      if (letterGuesses === wordArray[0]) {
-        letterGuesses.push(wordArray);
-        console.log(' ' + letterGuesses +  ' ');
-      } 
-      else {
-        console.log('Guess again!');
+    for (var i = 0; i < chosenWord.length; i++) {
+      if (this.lettersInWord[i] === this.letter) {
+
+      // letter match? then, set bool to TRUE
+      this.letterGuessed = true;
+      console.log("Your letter guess " + res.letterGuessed + " is correct!");
+
+    } else {
+      console.log("Your letter guess " + res.letterGuessed + " is not correct! Guess again!");
+    }
+  }
+
+  // letter is indiced in chosenWord then loop through and populate 
+  if (this.letterGuessed) {
+
+    // Loop through the chosenWord which is all placeHolder 
+    for (var j = 0; j < placeHolder.length; j++) {
+
+      // Populate every instance of the letter
+      if (this.chosenWord[j] === this.letter) {
+
+        // set specific blank spaces to equal the correct letter if match
+        this.placeHolder[j] = this.letter;
       }
-
-      // pushes newguy object into our array
-      wordArray.push(letterGuesses);
+    }
+    // Log the current state of chosenWord
+    console.log(letterAndPlacholderUpdated);
+  }
       // add one to count to increment our recursive loop by one
-
-      count++;
-      // run the askquestion function again so as to either end the loop or ask the questions again
-      askQuestion();
+      numberOfGuesses++;
+      // run the askquestion function again - either end the loop or ask the questions again
+      startGame();
     });
     // else statement which runs a for loop that will execute .printInfo() for each object inside of our array
   }
-  else {
-    for (var x = 0; x < wordArray.length; x++) {
-      wordArray[x].printInfo();
-    }
-  }
-};
-
 // call askQuestion to run our code
-askQuestion();
+startGame();
+
+ function endGame() {
+ if (lettersInWord.toString() === placeHolder.toString()) {
+    alert("You win! PLease play again!");
+    // Restart the game
+    startGame();
+
+  } else if (numberOfGuesses === 0) {
+
+    inquirer.prompt([
+      {
+      type: 'input',
+      message: 'You are now all out of guesses. Would you like to play again?',
+      name: "confirm",
+      default: true
+    },
+  ])
+    .then(function(err, res) {
+
+    if (res.confirm) {
+      console.log("\nGreat, a new hangman game will begin!\n");
+      // Restart the game
+      startGame();
+    } 
+
+    else {
+      console.log("\nPlay again when you are ready!\n");
+      }
+    });
+  }
+}
+
+
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,34 +1,37 @@
 // dependency for inquirer npm package
 var inquirer = require("inquirer");
 var prompt = require("prompt");
+// Using the require keyword lets us access all of the exports in our word.js file
+var letter = require("./letter.js");
 
+var wordSelection = ["trace scheduling", "fortran", "formatting objects processor", 
+"fourier transform", "high level assembly", "instruction set architecture", 
+"sparse conditional constant propagation", "design compiler"];
 
-var wordOptions = {
-  words: ["engineering", "bluetooth", "electricity", "amperes", "electrons", "gate"]
-};
+// randomized word chosen from wordOptons[]
+var chosenWord = wordSelection[Math.floor(Math.random() * wordSelection.length)];
 
-// module.exports is essentially an object that we can add data or variables to
-// We can access them from other files using the 'require' keyword.
+// number of "__ __"  so when I call placeHolder I can have it === "__"
+var placeHolder = lettersInWord.length;
 
+// split word and store in var
+var lettersInWord = chosenWord.split("");
 
-
-
-
-
-// THIS IS THE OTHER OPTION: 
-
-// constructor function used to create programmer objects
-function Word(letter, position) {
-  this.letter = letter;
-  this.position = position;
+// constructor function used to create objects
+function Word(chosenWord, lettersInWord) {
+  this.chosenWord = chosenWord;
+  this.lettersInWord = lettersInWord;
 }
 
-// creates the printInfo method and applies it to all programmer objects
-Word.prototype.printInfo = function() {
-  console.log("__" + this.letter + "__" + this.position);
+// placeholders for all words in wordSelection - starts each game
+Word.prototype.placeHolder = function() {
+  // based on # of letters in correctWord.
+for (var i = 0; i < lettersInWord.length; i++) {
+  placeHolder.push("_");
+  }
 };
 
-
-
-
-module.exports = wordOptions;
+// module.exports is an object that we can add data or variables to
+// We can access them from other files using the 'require' keyword.
+module.exports = wordSelection;
+module.exports = Word;
